d1e65ee4163ba696894b35fccdb00c374e508bd9
noobgamer75 <87486144+noobgamer75@users.noreply.github.com>
2024-01-06 13:55:34 +0530

    Add files via upload

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -1,60 +1,12 @@
-// import { Client, Account,ID } from "appwrite";
-import sdk from "node-appwrite";
 import {Client,Account,ID,Functions} from "appwrite"
-
-const clientServer = new sdk.Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('6586f950009ee7b0ca43')              // Your project ID
-    .setKey("bcfc3d723213e2c86b82a1ac0ee0a14678b78419d095ceba58ea02b16504906d82675694ec3bbc8b1d12705bae4b7ac6440e6898c3da771ab97933bfe88dcec76fe04d8cf7f0bcb871dcbae42c355f311d47f320c73154301fb3724d47f1259686dbdf6d046ab3414fc0ab924e69c65b8b37002c4daea4d775b392389e14efbd")
-const accountServer = new sdk.Account(clientServer);
 
 const client = new Client()
 .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
 .setProject('6586f950009ee7b0ca43')              // Your project ID
 
 const account = new Account(client);
-const functions = new Functions(client)
-// console.log("Client: ", client);
-// console.log("Account: ", account);
 
-// login and create new session
-export const loginWithEmailPassword= async(email,password) =>{
-    try {
-        const response = await account.createEmailSession(email, password);
-        console.log(response); // Success
-        return response;
-    } catch (error) {
-        console.log(error); // Failure
-        throw error;
-    }
-    
-}
-
-// create new account
-export const createAccount = async (email, password, name) => {
-    try {
-        const response = await account.create(ID.unique(), email, password, name);
-        console.log(response); // Success
-        return response;
-    } catch (error) {
-        console.log(error); // Failure
-        throw error;
-    }
-};
-
-export const createNewVerification = async () => {
-    try {
-        const response = await account.createVerification('https://localhost:3000/verifyemail');
-        console.log(response); // Success
-        return response;
-    }
-    catch (error) {
-        console.log(error); // Failure
-        throw error;
-    }
-}
-
-// complete verification mail to user (client)
+// complete verification for the client
 export const updateVerification = async (userId, secret) => {
     try{
        const response=await account.updateVerification(userId, secret);
@@ -67,92 +19,10 @@ export const updateVerification = async (userId, secret) => {
     }
 }
 
-// logout
-export const logout =async () =>{
-    try{
-        const response = await account.deleteSession('65891a6e381e07d4d34f');
-        console.log(response); // Success
-        return response;
-    }
-    catch(error){
-        console.log(error); // Failure
-        throw error;
-    }
-}
-
-// get current session
-export const getCurrentSession = async (sessionId) => {
+// update and reset the password
+export const updateNewPassword = async (userId, secret, password,password_confirm) => {
     try {
-        const response = await account.getSession(sessionId);
-        console.log(response); // Success
-        return response;
-    } catch (error) {
-        console.log(error); // Failure
-        throw error;
-    }
-};
-export const deleteCurrentSession = async (sessionId) => {
-    try {
-        const response = await account.deleteSession(sessionId);
-        console.log(response); // Success
-        return response;
-    } catch (error) {
-        console.log(error); // Failure
-        throw error;
-    }
-};
-
-
-// create new account with OAuth2 Google
-export const continueWithGoogle = (req,res) =>{
-    const {code} = req.body;
-    const promise = account.createOAuth2Session('google', code);
-    promise.then(function (response) {
-        console.log(response); // Success
-    }, function (error) {
-        console.log(error); // Failure
-    });
-    res.send("Create new account with OAuth2 Google")
-}
-
-// execute a cloud funtion in client
-export const executeCloudFunction = async () => {
-    
-    try {
-        const execution = await functions.createExecution(
-            '658c63c6f33e70d6308a',
-            JSON.stringify({ 'foo': 'bar' }),
-            false,
-            '/test',
-            'POST',
-            { 'X-Custom-Header': '123' }
-        )
-        console.log(execution)
-        return execution.responseBody;
-    } catch (err) {
-        console.error(err.message)
-        throw err;
-    }
-};
-
-// server
-// recover password with email in server
-export const sendPasswordResetMail =async (email)=>{
-    try{
-    const response = await accountServer.createRecovery(email,'https://localhost:3000/recovery');
-    console.log(response); // Success
-    return response;
-    }
-    catch(error){
-        console.log(error); // Failure
-        throw error;
-    }
-}
-
-// update password in server
-export const updatePassword = async (userId, secret, password,password_confirm) => {
-    try {
-        const response = await accountServer.updateRecovery(userId, secret, password,password_confirm);
+        const response = await account.updateRecovery(userId, secret, password,password_confirm);
         console.log(response); // Success
         return response;
     } catch (error) {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { loginWithEmailPassword,createAccount ,logout,getCurrentSession,updatePassword,sendPasswordResetMail, updateVerification,executeCloudFunction} from "./controllers/auth_controllers.js"
+import {updateNewPassword,updateVerification} from "./controllers/auth_controllers.js"
 
 const app = express()
 app.use(cors())
@@ -14,84 +14,7 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-// client login
-app.post("/login", async(req, res) => {
-    const { email, password } = req.body;
-    try{
-        const result = await loginWithEmailPassword(email, password); // Wait for loginWithEmailPassword function to complete
-        res.send(result);
-    }catch(e){
-        res.status(400).send(e);
-    }
-})
-
-// client register
-app.post("/register", async (req, res) => {
-    const { email, password, name } = req.body;
-    console.log(email, password, name);
-    try {
-        const result = await createAccount(email, password, name); // Wait for createAccount function to complete
-        res.send(result);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-// verify email address
-app.post("/verifymail", async (req, res) => {
-    const { userId, secret } = req.body;
-    try {
-        const result = await account.updateVerification(userId, secret); // Wait for updateVerification function to complete
-        res.send(result);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-
-app.post("/session", async(req, res) => {
-    const sessionId=req.body.sessionId;
-    try{
-        const result = await getCurrentSession(sessionId); // Wait for currentSession function to complete
-        res.send(result);
-    }catch(e){
-        res.status(400).send(e);
-    }
-})
-
-app.post("/logout", async(req, res) => {
-    const { sessionId } = req.body;
-    try{
-        const result = await logout(); // Wait for logout function to complete
-        res.send(result);
-    }catch(e){
-        res.status(400).send(e);
-    }
-})
-
-app.get("/recovery", (req, res) => {
-    const {userId,secret}=req.query;
-    console.log("userId",userId);
-    console.log("secret",secret);
-    res.render("reset_password",{userId,secret,message:""});
-});
-
-app.post("/reset_password", async (req, res) => {
-    const { userId, secret, password, password_confirm } = req.body;
-
-    if (password !== password_confirm) {
-        res.render("reset_password",{userId,secret, message:"Passwords do not match."});
-    }
-
-    try {
-        const result = await  updatePassword(userId,secret,password,password_confirm); // Wait for updatePassword function to complete
-        console.log(result);
-        res.render("template",{title:"Password Reset", message:"Your password has been reset successfully."});
-    } catch (err) {
-        res.render("template",{title:"Password Reset Failed", message:"Your password reset failed."});
-    }
-});
-
+// email veriifcation endpoint
 app.get("/verify",async(req,res)=>{
     const {userId,secret}=req.query;
     console.log("userId",userId);
@@ -100,31 +23,45 @@ app.get("/verify",async(req,res)=>{
     try{
         const result = await updateVerification(userId, secret); // Wait for updateVerification function to complete
         console.log(result);
-        res.render("template",{title:"Verification Complete", message:"Your email address has been verified successfully."});
+        res.render("template",{title:"✅ Verification Complete", message:"Your email address has been verified successfully.",});
     }
     catch(e){
-        res.render("template",{title:"Verification Failed", message:"Your email address verification failed."});
+        res.render("template",{title:"❌ Verification Failed", message:`⚠️ Reason : ${e.message}`,});
     }
 })
 
-// success or failure status
-app.get("/success", (req, res) => {
-    res.render("template",{title:"Success", message:"Your request has been completed successfully."});
+// password reset endpoint
+app.get("/recovery", (req, res) => {
+    const {userId,secret}=req.query;
+    console.log("userId",userId);
+    console.log("secret",secret);
+    res.render("reset_password",{userId,secret,message:""});
 });
 
-app.get("/failure", (req, res) => {
-    res.render("template",{title:"Failure", message:"Your request has failed."});
-});
+// complete password reset post endpoint
+app.post("/reset_password", async (req, res) => {
+    const { userId, secret, password, password_confirm } = req.body;
 
-// run function
-app.get("/run", async (req, res) => {
-    const { functionName, functionData } = req.query;
-    try {
-        const result = await executeCloudFunction(); // Wait for executeCloudFunction function to complete
-        res.send(result);
-    } catch (err) {
-        res.status(400).send(err);
+    if (password !== password_confirm) {
+        res.render("reset_password",{userId,secret, message:"Passwords do not match."});
     }
+
+    if (password.length < 8) {
+        res.render("reset_password",{userId,secret, message:"Password must be at least 8 characters."});
+    }
+
+    try {
+        const result = await  updateNewPassword(userId,secret,password,password_confirm); // Wait for updatePassword function to complete
+        console.log(result);
+        res.render("template",{title:"✅ Password Changed", message:"Your password was changed successfully.",});
+    } catch (err) {
+        res.render("template",{title:"❌ Password Reset Failed", message:`⚠️ Reason : ${err.message}`,});
+    }
+});
+
+// 404 error page
+app.get("*", (req, res) => {
+    res.render("template",{title:"❌ Error", message:"⚠️ Page not found",});
 });
 
 app.listen(3000, () => {
